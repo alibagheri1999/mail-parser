438d8e09983af8a839539fc253f51d6e6a52a077
ali bagheri <85070715+alibagheri1999@users.noreply.github.com>
2023-08-04 15:22:54 +0330

    add env validator

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { configValidationSchema } from './schema/env.validator';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env'],
       expandVariables: true,
+      validationSchema: configValidationSchema,
     }),
     ThrottlerModule.forRoot({
       ttl: 3600 * 12,
